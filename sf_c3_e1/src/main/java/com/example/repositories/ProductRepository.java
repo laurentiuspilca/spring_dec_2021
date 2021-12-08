9a327c289457d96b6c73be5e683febf3fefa517d
Laurentiu Spilca <Laurentiu.Spilca@endava.com>
2021-12-08 11:10:12 +0200

    C3

--- sf_c3_e1/src/main/java/com/example/repositories/ProductRepository.java
+++ sf_c3_e1/src/main/java/com/example/repositories/ProductRepository.java
@@ -2,6 +2,12 @@ package com.example.repositories;
 
 import com.example.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+  
+  @Query("SELECT p FROM Product p WHERE p.name = :name")
+  Optional<Product> findProductByName(String name);
 }
